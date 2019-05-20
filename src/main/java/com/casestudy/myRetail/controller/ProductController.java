0015294c017a7f46847ddfda5c5a32ae3c95adfb
Shashank Tripathi <shashank.tripathi@go-jek.com>
2019-05-20 18:44:48 +0530

    added controller layer for put request for price update

--- src/main/java/com/casestudy/myRetail/controller/ProductController.java
+++ src/main/java/com/casestudy/myRetail/controller/ProductController.java
@@ -1,13 +1,11 @@
 package com.casestudy.myRetail.controller;
 
+import com.casestudy.myRetail.request.PriceUpdateRequest;
 import com.casestudy.myRetail.response.ProductResponse;
 import com.casestudy.myRetail.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -17,7 +15,13 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping(value = "/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ProductResponse getProduct(@PathVariable Long productId) {
+    public @ResponseBody ProductResponse getProduct(@PathVariable Long productId) {
         return productService.getProduct(productId);
+    }
+
+    @PutMapping(value = "/{productId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody ProductResponse updateProductPrice(@PathVariable Long productId,
+                                                            @RequestBody PriceUpdateRequest request) {
+        return productService.updateProductPrice(productId, request);
     }
 }
--- src/test/java/com/casestudy/myRetail/controller/ProductControllerTest.java
+++ src/test/java/com/casestudy/myRetail/controller/ProductControllerTest.java
@@ -1,13 +1,17 @@
 package com.casestudy.myRetail.controller;
 
+import com.casestudy.myRetail.exception.InvalidUpdateException;
 import com.casestudy.myRetail.exception.ProductNotFoundException;
 import com.casestudy.myRetail.exception.ProductPriceDbException;
 import com.casestudy.myRetail.model.enums.CurrencyCodeEnum;
+import com.casestudy.myRetail.request.PriceUpdateRequest;
+import com.casestudy.myRetail.request.ProductPriceRequest;
 import com.casestudy.myRetail.response.ProductPriceResponse;
 import com.casestudy.myRetail.response.ProductResponse;
 import com.casestudy.myRetail.response.RedSkyProductResponse;
 import com.casestudy.myRetail.service.ProductPriceService;
 import com.casestudy.myRetail.service.RedSkyProductService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -24,6 +29,7 @@ import java.math.BigDecimal;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -59,6 +65,14 @@ public class ProductControllerTest {
             .id(PRODUCT_ID)
             .name(PRODUCT_NAME)
             .currentPrice(PRODUCT_PRICE_RESPONSE)
+            .build();
+    private static final PriceUpdateRequest PRICE_UPDATE_REQUEST = PriceUpdateRequest.builder()
+            .id(PRODUCT_ID)
+            .name(PRODUCT_NAME)
+            .currentPrice(ProductPriceRequest.builder()
+                    .value(PRICE_VALUE)
+                    .currencyCode(CURRENCY.getCode())
+                    .build())
             .build();
 
     @Test
@@ -104,5 +118,62 @@ public class ProductControllerTest {
                 .andDo(print())
                 .andExpect(status().is5xxServerError())
                 .andReturn();
+    }
+
+    @Test
+    public void shouldReturnSuccessReponseForUpdateWhenValidData() throws Exception {
+        when(productPriceService.updatePrice(PRODUCT_ID, PRICE_UPDATE_REQUEST)).thenReturn(PRODUCT_PRICE_RESPONSE);
+
+        MvcResult result = this.mockMvc.perform(put("/products/" + PRODUCT_ID)
+                .content(getProductPriceUpdateRequest())
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        String actualResponse = result.getResponse().getContentAsString();
+        Assert.assertEquals(objectMapper.writeValueAsString(PRODUCT_RESPONSE), actualResponse);
+    }
+
+    @Test
+    public void shouldReturn404ReponseForUpdateWhenProductDoesNotExists() throws Exception {
+        when(productPriceService.updatePrice(PRODUCT_ID, PRICE_UPDATE_REQUEST))
+                .thenThrow(new ProductNotFoundException(""));
+
+        this.mockMvc.perform(put("/products/" + PRODUCT_ID)
+                .content(getProductPriceUpdateRequest())
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+
+    @Test
+    public void shouldReturn400ReponseForUpdateWhenProductIdMismatch() throws Exception {
+        when(productPriceService.updatePrice(PRODUCT_ID, PRICE_UPDATE_REQUEST))
+                .thenThrow(new InvalidUpdateException(""));
+
+        this.mockMvc.perform(put("/products/" + PRODUCT_ID)
+                .content(getProductPriceUpdateRequest())
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+    @Test
+    public void shouldReturn400ReponseForUpdateWhenProductPriceDbIsDown() throws Exception {
+        when(productPriceService.updatePrice(PRODUCT_ID, PRICE_UPDATE_REQUEST))
+                .thenThrow(new ProductPriceDbException(""));
+
+        this.mockMvc.perform(put("/products/" + PRODUCT_ID)
+                .content(getProductPriceUpdateRequest())
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is5xxServerError())
+                .andReturn();
+    }
+
+    private String getProductPriceUpdateRequest() throws JsonProcessingException {
+        return objectMapper.writeValueAsString(PRICE_UPDATE_REQUEST);
     }
 }
